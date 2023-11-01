8690d4bf7f3ab5b89fa927323e4c69aa3b456eea
alaminrobot1 <149223128+alaminrobot1@users.noreply.github.com>
2023-11-01 09:48:15 +0600

    Create gateio.py

---
+++ exchanges/gateio.py
@@ -0,0 +1,46 @@
+import ccxt
+
+gateio = ccxt.gateio()
+
+def get_exchange_data():
+    try:
+        gateio_markets = gateio.load_markets()
+        gateio_spot_markets = {symbol: market for symbol, market in gateio_markets.items() if market['spot'] and market['active']}
+        gateio_tickers = gateio.fetch_tickers(list(gateio_spot_markets.keys()))
+
+        # Initialize dictionaries to store order book, deposit/withdraw, trading fee, and withdrawal fee data
+        order_books = {}
+        deposit_withdraw_info = {}
+        trading_fees = {}
+        withdrawal_fees = {}
+
+        # Loop through all available trading pairs
+        for symbol in gateio_spot_markets:
+            # Retrieve order book data for the current trading pair
+            order_books[symbol] = gateio.fetch_order_book(symbol)
+
+            # Retrieve deposit/withdraw network information for the current trading pair's assets
+            base_asset, quote_asset = symbol.split('/')
+            if base_asset not in deposit_withdraw_info:
+                deposit_withdraw_info[base_asset] = gateio.fetch_deposit_and_withdraw(base_asset)
+            if quote_asset not in deposit_withdraw_info:
+                deposit_withdraw_info[quote_asset] = gateio.fetch_deposit_and_withdraw(quote_asset)
+
+            # Retrieve trading fees for the current trading pair
+            trading_fees[symbol] = gateio.fetch_trading_fees(symbol)
+
+            # Retrieve withdrawal fees for the current trading pair's assets
+            if base_asset not in withdrawal_fees:
+                withdrawal_fees[base_asset] = gateio.fetch_withdraw_fees(base_asset)
+            if quote_asset not in withdrawal_fees:
+                withdrawal_fees[quote_asset] = gateio.fetch_withdraw_fees(quote_asset)
+
+        return {
+            'tickers': gateio_tickers,
+            'order_books': order_books,
+            'deposit_withdraw_info': deposit_withdraw_info,
+            'trading_fees': trading_fees,
+            'withdrawal_fees': withdrawal_fees
+        }
+    except Exception as e:
+        return None, "Error retrieving data from Gate.io API: {}".format(e)
