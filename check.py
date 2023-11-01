ebb33065bd19d4ab8dcd29f9e4e4e77079aa24f8
alaminrobot1 <149223128+alaminrobot1@users.noreply.github.com>
2023-11-01 16:39:51 +0600

    Update check.py

--- check.py
+++ check.py
@@ -1,16 +1,25 @@
 import ccxt
+import importlib
+import json
+from config import EXCHANGES
 
-def check_liquidity_and_network(exchange, symbol):
+def check_liquidity_and_network(exchange_key, symbol):
     """
     Checks the liquidity and deposit/withdraw network availability for a given symbol on an exchange.
 
     Args:
-        exchange: The exchange object.
+        exchange_key: The exchange key.
         symbol: The symbol to check.
 
     Returns:
         A tuple of two booleans, where the first boolean indicates whether or not the symbol has sufficient liquidity and the second boolean indicates whether or not the deposit/withdraw network is available for the symbol.
     """
+
+    exchange = EXCHANGES.get(exchange_key)
+    if not exchange:
+        return False, False
+
+    exchange_module = importlib.import_module(exchange['module'])
 
     # Check for liquidity
     order_book = exchange.fetchOrderBook(symbol)
@@ -26,4 +35,3 @@ def check_liquidity_and_network(exchange, symbol):
         deposit_withdraw_network_available = False
 
     return liquidity, deposit_withdraw_network_available
-
