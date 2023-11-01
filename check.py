cbc7e9ed7491a65e9b085fe71cc3fb475abbf4cf
alaminrobot1 <149223128+alaminrobot1@users.noreply.github.com>
2023-11-01 23:31:36 +0600

    Update check.py

--- check.py
+++ check.py
@@ -1,37 +1,26 @@
 import ccxt
-import importlib
-import json
-from config import EXCHANGES
 
 def check_liquidity_and_network(exchange_key, symbol):
-    """
-    Checks the liquidity and deposit/withdraw network availability for a given symbol on an exchange.
-
-    Args:
-        exchange_key: The exchange key.
-        symbol: The symbol to check.
-
-    Returns:
-        A tuple of two booleans, where the first boolean indicates whether or not the symbol has sufficient liquidity and the second boolean indicates whether or not the deposit/withdraw network is available for the symbol.
-    """
-
-    exchange = EXCHANGES.get(exchange_key)
-    if not exchange:
+    exchange_info = EXCHANGES.get(exchange_key)
+    if not exchange_info:
         return False, False
 
-    exchange_module = importlib.import_module(exchange['module'])
+    # Create a CCXT exchange instance based on the exchange's module name
+    exchange = importlib.import_module(exchange_info['module'])
 
-    # Check for liquidity
-    order_book = exchange.fetchOrderBook(symbol)
-    liquidity = order_book['asks'][0][1] + order_book['bids'][0][1]
+    try:
+        order_book = exchange.fetchOrderBook(symbol)
+        liquidity = order_book['asks'][0][1] + order_book['bids'][0][1]
 
-    # Check for deposit/withdraw network availability
-    deposit_methods = exchange.fetchDepositMethods(symbol)
-    withdrawal_methods = exchange.fetchWithdrawalMethods(symbol)
+        deposit_methods = exchange.fetchDepositMethods(symbol)
+        withdrawal_methods = exchange.fetchWithdrawalMethods(symbol)
 
-    if symbol in deposit_methods and symbol in withdrawal_methods:
-        deposit_withdraw_network_available = True
-    else:
-        deposit_withdraw_network_available = False
+        if symbol in deposit_methods and symbol in withdrawal_methods:
+            deposit_withdraw_network_available = True
+        else:
+            deposit_withdraw_network_available = False
 
-    return liquidity, deposit_withdraw_network_available
+        return liquidity, deposit_withdraw_network_available
+
+    except Exception as e:
+        return False, False
