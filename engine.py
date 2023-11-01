8616327c76328922f67e90d7870552089b3d2be5
alaminrobot1 <149223128+alaminrobot1@users.noreply.github.com>
2023-11-01 15:32:21 +0600

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,75 @@
+from importlib import import_module
+from config import EXCHANGES
+
+from check import check_liquidity_and_network
+
+def get_exchange_name(exchange_key: str) -> str:
+  return EXCHANGES[exchange_key]['name']
+
+def get_exchange_module(exchange_key: str) -> exchange.Exchange:
+  module_name = EXCHANGES[exchange_key]['module']
+  return import_module(module_name)
+
+def calculate_arbitrage(exchange1: exchange.Exchange, exchange2: exchange.Exchange) -> List[dict]:
+  """Calculates the arbitrage opportunities between two exchanges.
+
+  Args:
+    exchange1: An Exchange object representing the first exchange.
+    exchange2: An Exchange object representing the second exchange.
+
+  Returns:
+    A list of arbitrage opportunities.
+  """
+
+  exchange1_config = EXCHANGES.get(exchange1)
+  exchange2_config = EXCHANGES.get(exchange2)
+
+  if not exchange1_config or not exchange2_config:
+    return []
+
+  exchange1_module = get_exchange_module(exchange1_config['module'])
+  exchange2_module = get_exchange_module(exchange2_config['module'])
+
+  exchange1_tickers = exchange1_module.get_exchange_data()
+  exchange2_tickers = exchange2_module.get_exchange_data()
+
+  exchange1_trade_base_url = exchange1_config['trade_base_url']
+  exchange2_trade_base_url = exchange2_config['trade_base_url']
+
+  common_symbols = set(exchange1_tickers.keys()) & set(exchange2_tickers.keys())
+
+  data = []
+  for symbol in common_symbols:
+
+    # Check for liquidity and deposit/withdraw network availability on both exchanges
+    liquidity1, deposit_withdraw_network_available1 = check_liquidity_and_network(exchange1, symbol)
+    liquidity2, deposit_withdraw_network_available2 = check_liquidity_and_network(exchange2, symbol)
+
+    # Only include arbitrage opportunities where both exchanges have sufficient liquidity and have deposit/withdraw network availability
+    if liquidity1 > 0 and deposit_withdraw_network_available1 and liquidity2 > 0 and deposit_withdraw_network_available2:
+      exchange1_price = float(exchange1_tickers[symbol]['last'])
+      exchange2_price = float(exchange2_tickers[symbol]['last']) if exchange2_tickers[symbol]['last'] is not None else 0.0
+      arbitrage = round((exchange2_price - exchange1_price) / exchange1_price * 100, 2)
+
+      # Exchange trade link generation
+      exchange1_symbol_link = symbol.replace('/', '_') if exchange1 in ('exchanges.mexc', 'exchanges.gateio', 'exchanges.binance') else symbol.replace('/', '/')
+      exchange2_symbol_link = symbol.replace('/', '_') if exchange2 in ('exchanges.mexc', 'exchanges.gateio', 'exchanges.binance') else symbol.replace('/', '/')
+
+      exchange1_trade_link = f"{exchange1_trade_base_url}{exchange1_symbol_link}"
+      exchange2_trade_link = f"{exchange2_trade_base_url}{exchange2_symbol_link}"
+
+      data.append({
+        'symbol': symbol,
+        'exchange1_price': exchange1_price,
+        'exchange2_price': exchange2_price,
+        'arbitrage': arbitrage,
+        'exchange1_trade_link': exchange1_trade_link,
+        'exchange2_trade_link': exchange2_trade_link,
+        'exchange1_name': exchange1_config['name'],
+        'exchange2_name': exchange2_config['name']
+      })
+
+  # Sort data by arbitrage value
+  data.sort(key=lambda x: x['arbitrage'], reverse=True)
+
+  return data
