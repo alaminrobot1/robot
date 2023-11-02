92e3f3f3a33c034f80d3f2debb6d1cc9ea07811c
alaminrobot1 <149223128+alaminrobot1@users.noreply.github.com>
2023-11-02 10:39:31 +0600

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 from importlib import import_module
-import json
 from config import EXCHANGES
 from check import check_liquidity_and_network
+from flask import request
 
 def get_exchange_name(exchange_key):
     return EXCHANGES[exchange_key]['name']
@@ -23,29 +23,29 @@ def calculate_arbitrage(exchange1, exchange2, min_liquidity):
     exchange1_tickers = exchange1_module.get_exchange_data()
     exchange2_tickers = exchange2_module.get_exchange_data()
 
-    exchange1_trade_base_url = exchange1_config['trade_base_url']
-    exchange2_trade_base_url = exchange2_config['trade_base_url']
-
-    common_symbols = set(exchange1_tickers.keys()) & set(exchange2_tickers.keys())
+    common_symbols = set(exchange1_tickers.keys()) & set(exchange2_tickers.keys()
 
     data = []
     for symbol in common_symbols:
-        liquidity1, deposit_withdraw_network_available1 = check_liquidity_and_network(exchange1, symbol)
-        liquidity2, deposit_withdraw_network_available2 = check_liquidity_and_network(exchange2, symbol)
+        # Check for liquidity on both exchanges
+        liquidity1 = check_liquidity(exchange1, symbol)
+        liquidity2 = check_liquidity(exchange2, symbol)
 
-        # Check if both exchanges have sufficient liquidity and meet the minimum liquidity criteria
-        if liquidity1 > 0 and liquidity1 >= min_liquidity and deposit_withdraw_network_available1 and \
-           liquidity2 > 0 and liquidity2 >= min_liquidity and deposit_withdraw_network_available2:
+        # Only include arbitrage opportunities where both exchanges have sufficient liquidity
+        if liquidity1 >= min_liquidity and liquidity2 >= min_liquidity:
+
             exchange1_price = float(exchange1_tickers[symbol]['last'])
             exchange2_price = float(exchange2_tickers[symbol]['last']) if exchange2_tickers[symbol]['last'] is not None else 0.0
             arbitrage = round((exchange2_price - exchange1_price) / exchange1_price * 100, 2)
 
+            # Add new updates
             # Calculate the spread between the two exchanges
             spread = round((exchange2_price - exchange1_price) / (exchange1_price + exchange2_price) * 2 * 100, 2)
 
             # Calculate the arbitrage opportunity in percentage terms
             arbitrage_opportunity = round(arbitrage - spread, 2)
 
+            # Add the new fields to the data dictionary
             data.append({
                 'symbol': symbol,
                 'exchange1_price': exchange1_price,
@@ -53,8 +53,8 @@ def calculate_arbitrage(exchange1, exchange2, min_liquidity):
                 'arbitrage': arbitrage,
                 'spread': spread,
                 'arbitrage_opportunity': arbitrage_opportunity,
-                'exchange1_trade_link': exchange1_trade_base_url + symbol.replace('/', '_'),
-                'exchange2_trade_link': exchange2_trade_base_url + symbol.replace('/', '_'),
+                'exchange1_trade_link': exchange1_config['trade_base_url'] + symbol.replace('/', '_'),
+                'exchange2_trade_link': exchange2_config['trade_base_url'] + symbol.replace('/', '_'),
                 'exchange1_name': exchange1_config['name'],
                 'exchange2_name': exchange2_config['name']
             })
